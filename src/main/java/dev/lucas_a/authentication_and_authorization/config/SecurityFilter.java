e4ae76e155b6bcbe633e60c9ba063030859fa1ac
Lucas Tadeu Almeida Martins de Oliveira <143283670+LucasAlmeidaBr97@users.noreply.github.com>
2026-08-02 18:15:07 -0300

    SecurityFilter Implementado
    Foi feita a implementação do securityFilter para filtrar as resquições que exigem autenticação JWT

---
+++ src/main/java/dev/lucas_a/authentication_and_authorization/config/SecurityFilter.java
@@ -0,0 +1,47 @@
+package dev.lucas_a.authentication_and_authorization.config;
+
+import java.io.IOException;
+import java.util.Optional;
+
+import org.apache.logging.log4j.util.Strings;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    private final TokenConfig tokenConfig;
+
+    public SecurityFilter(TokenConfig tokenConfig) {
+        this.tokenConfig = tokenConfig;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        String authorizeHeader = request.getHeader("Authorization");
+        if (Strings.isNotEmpty(authorizeHeader) && authorizeHeader.startsWith("Bearer")) {
+            String token = authorizeHeader.substring("Bearer ".length());
+            Optional<JWTUserData> optUser = tokenConfig.validateToken(token);
+            if (optUser.isPresent()) {
+                JWTUserData userData = optUser.get();
+                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userData,
+                        null, null);
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+
+            }
+
+            filterChain.doFilter(request, response);
+        } else {
+            filterChain.doFilter(request, response);
+        }
+    }
+
+}
--- src/main/java/dev/lucas_a/authentication_and_authorization/config/TokenConfig.java
+++ src/main/java/dev/lucas_a/authentication_and_authorization/config/TokenConfig.java
@@ -1,28 +1,48 @@
 package dev.lucas_a.authentication_and_authorization.config;
 
 import java.time.Instant;
+import java.util.Optional;
 
 import org.springframework.stereotype.Component;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 
 import dev.lucas_a.authentication_and_authorization.entity.User;
 
 @Component
 public class TokenConfig {
 
-    
     private String secret = "secret";
 
     Algorithm algorithm = Algorithm.HMAC256(secret);
 
-    public String generateToken(User user){
+    public String generateToken(User user) {
         return JWT.create()
-            .withClaim("userId", user.getId())
-            .withSubject(user.getEmail())
-            .withExpiresAt(Instant.now().plusSeconds(86400))
-            .withIssuedAt(Instant.now())
-            .sign(algorithm);
+                .withClaim("userId", user.getId())
+                .withSubject(user.getEmail())
+                .withExpiresAt(Instant.now().plusSeconds(86400))
+                .withIssuedAt(Instant.now())
+                .sign(algorithm);
     }
+
+    public Optional<JWTUserData> validateToken(String token) {
+
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+
+            DecodedJWT decode = JWT.require(algorithm)
+                    .build().verify(token);
+
+            return Optional.of(JWTUserData.builder()
+                    .userId(decode.getClaim("userId").asLong())
+                    .email(decode.getSubject())
+                    .build());
+        } catch (JWTVerificationException e) {
+            return Optional.empty();
+        }
+    }
+
 }
